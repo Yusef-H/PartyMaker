7758e531d4d53d4ef18370b458e6ca9606b237be
Natifishman <nati562000@gmail.com>
2025-08-09 00:43:51 +0300

    fix(phase 2): update adapters

--- app/src/main/java/com/example/partymaker/ui/adapters/UserAdapter.java
+++ app/src/main/java/com/example/partymaker/ui/adapters/UserAdapter.java
@@ -1,6 +1,5 @@
 package com.example.partymaker.ui.adapters;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
